f633990f50b34118671d5135af21468c814a25da
Ivan Candido <ivan.candido@powerhrg.com>
2021-07-07 17:04:56 -0400

    Task and Task Filter Deliverable done

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, onDelete }) {
+  const handleDeleteClick = (text) => {
+    const filteredTasks = tasks.filter(task => task.text !== text)
+    onDelete(filteredTasks)
+    console.log(filteredTasks)
+  }
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (
+        <Task 
+          key={task.text}
+          text={task.text}
+          category={task.category}
+          onDeleteClick={handleDeleteClick}
+        />))
+      }
     </div>
   );
 }
